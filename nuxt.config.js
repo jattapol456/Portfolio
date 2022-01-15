5b331a3b689fc00fe1bdba7bf1a20bee8ffd14d2
jattapol456 <jattapol.aom@gmail.com>
2022-01-16 05:02:31 +0700

    feat: aboutaward

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Jattapol AOMJ',
+    title: 'Jattapol Aomj',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
